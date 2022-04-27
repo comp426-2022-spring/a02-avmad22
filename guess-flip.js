3265a91e3f992d8569c262f8e057f6114a344d7e
avmad22 <avmad22@live.unc.edu>
2022-04-27 00:50:26 -0400

    minimist, again

--- guess-flip.js
+++ guess-flip.js
@@ -2,7 +2,7 @@ import { coinFlip, coinFlips, countFlips, flipACoin } from "./modules/coin.mjs";
 import minimist from 'minimist';
 let args=minimist(process.argv.slice(2));
 let call = args['call'];
-if(args.length ==2) { console.log("Error: no input.")} else if (call!="heads" && call!="tails") {
+if(call==null) { console.log("Error: no input.")} else if (call!="heads" && call!="tails") {
     console.log("Error: invalid input.")
     process.exit();
 } else {

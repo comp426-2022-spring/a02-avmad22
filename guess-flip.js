39f6c2db2f769bd3ae075e164f4defb225b7ec7e
avmad22 <avmad22@live.unc.edu>
2022-04-27 01:08:54 -0400

    gues flip confusion

--- guess-flip.js
+++ guess-flip.js
@@ -1,11 +1,11 @@
 import { coinFlip, coinFlips, countFlips, flipACoin } from "./modules/coin.mjs";
 import minimist from 'minimist';
-const args=minimist(process.argv.slice(2));
-let call = args['call'];
+const args= minimist(process.argv.slice(2))
+let call = args["call"];
 if((call==undefined) || (call!="heads" && call!="tails")) {
     console.log("Error: no input.");
     console.log("Usage: node guess-flip --call=[heads|tails]")
     
 } else {
-    console.log(flipACoin(call));
+    console.log(flipACoin(call))
 }

84e4b6fd3c4c93097aef3922aa99c9ce858d2244
avmad22 <avmad22@live.unc.edu>
2022-04-27 01:02:53 -0400

    gues flipper again

--- guess-flip.js
+++ guess-flip.js
@@ -2,11 +2,10 @@ import { coinFlip, coinFlips, countFlips, flipACoin } from "./modules/coin.mjs";
 import minimist from 'minimist';
 let args=minimist(process.argv.slice(2));
 let call = args['call'];
-if((call==null) || (call!="heads" && call!="tails")) {
-    console.log("Error: no input. \n Usage: node guess-flip --call=[heads|tails]")
+if((call==undefined) || (call!="heads" && call!="tails")) {
+    console.log("Error: no input.");
+    console.log("Usage: node guess-flip --call=[heads|tails]")
     
-} else if (call=="[heads|tails]"){
-    console.log(flipACoin(coinFlip()));
 } else {
     console.log(flipACoin(call));
 }

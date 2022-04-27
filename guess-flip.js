97b8b9c840fbd7dc440f577c2a22b97ef45d9571
avmad22 <avmad22@live.unc.edu>
2022-04-27 00:59:01 -0400

    gues flipper

--- guess-flip.js
+++ guess-flip.js
@@ -5,6 +5,8 @@ let call = args['call'];
 if((call==null) || (call!="heads" && call!="tails")) {
     console.log("Error: no input. \n Usage: node guess-flip --call=[heads|tails]")
     
+} else if (call=="[heads|tails]"){
+    console.log(flipACoin(coinFlip()));
 } else {
     console.log(flipACoin(call));
 }

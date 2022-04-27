88fa9955a020806dcc821bb9430580b988e5465c
avmad22 <avmad22@live.unc.edu>
2022-04-27 00:55:40 -0400

    gues flippy

--- guess-flip.js
+++ guess-flip.js
@@ -2,8 +2,8 @@ import { coinFlip, coinFlips, countFlips, flipACoin } from "./modules/coin.mjs";
 import minimist from 'minimist';
 let args=minimist(process.argv.slice(2));
 let call = args['call'];
-if(call==null) { console.log("Error: no input.")} else if (call!="heads" && call!="tails") {
-    console.log("Error: no input.")
+if((call==null) || (call!="heads" && call!="tails")) {
+    console.log("Error: no input. \n Usage: node guess-flip --call=[heads|tails]")
     
 } else {
     console.log(flipACoin(call));

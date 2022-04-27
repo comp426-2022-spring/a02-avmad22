46eac43ae59d883f89ddcf5a19f9346793ecec45
avmad22 <avmad22@live.unc.edu>
2022-04-27 00:30:27 -0400

    adding flips.js again

--- flips.js
+++ flips.js
@@ -1,5 +1,10 @@
 import { coinFlip, coinFlips, countFlips } from "./modules/coin.mjs";
 let args=process.argv;
+let array=[];
 if(args.length ==2) {
-    console.log(coinFlips(1));
-} else {console.log(coinFlips(args[2]))}
+    array = coinFlips(1);
+    console.log(array);
+    console.log(countFlips(array));
+} else {array = coinFlips(args[2]);
+    console.log(array);
+    console.log(countFlips(array));}

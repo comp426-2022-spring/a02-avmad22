cc084d621b9f2751a2525ec553f962a965cfbb48
avmad22 <avmad22@live.unc.edu>
2022-04-27 00:52:03 -0400

    guess

--- guess-flip.js
+++ guess-flip.js
@@ -3,8 +3,8 @@ import minimist from 'minimist';
 let args=minimist(process.argv.slice(2));
 let call = args['call'];
 if(call==null) { console.log("Error: no input.")} else if (call!="heads" && call!="tails") {
-    console.log("Error: invalid input.")
-    process.exit();
+    console.log("Error: no input.")
+    
 } else {
     console.log(flipACoin(call));
 }
